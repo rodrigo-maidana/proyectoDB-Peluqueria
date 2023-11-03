a38ffa2d024fa1082ef805a5fff09b857908c820
Jorge Figueredo <jorgefigueredo2001@gmail.com>
2023-11-03 20:02:53 -0300

    sumar_detalles_facturas mejorado

--- QueryProcedureProyectoPeluqueria.sql
+++ QueryProcedureProyectoPeluqueria.sql
@@ -1,5 +1,5 @@
 USE ProyectoPeluqueria
-
+drop procedure sumar_detalles_factura
 -- Crear un procedimiento almacenado para calcular y actualizar el total de una factura
 -- Crear un procedimiento almacenado para calcular y actualizar el total de una factura por número de factura
 -- Crear el procedimiento almacenado
@@ -9,6 +9,12 @@ AS
 BEGIN
     DECLARE @Total INT
     DECLARE @TotalIVA INT
+    DECLARE @CondicionCompra BIT
+
+    -- Obtener la condición de compra
+    SELECT @CondicionCompra = condicion_compra
+    FROM facturas
+    WHERE id_factura = @id_factura
 
     -- Calcular el total y el total del IVA para la factura
     SELECT @Total = SUM(cantidad * costo_unitario),
@@ -16,8 +22,15 @@ BEGIN
     FROM detalles_compras_proveedores
     WHERE id_factura = @id_factura
 
-    -- Actualizar los campos total y total_iva en la tabla facturas
+    -- Actualizar los campos total, total_iva y saldo_pendiente en la tabla facturas
     UPDATE facturas
-    SET total = @Total, total_iva = @TotalIVA
+    SET total = @Total, total_iva = @TotalIVA,
+        saldo_pendiente = CASE
+            WHEN @CondicionCompra = 1 THEN @Total
+            ELSE 0
+        END
     WHERE id_factura = @id_factura
 END;
+
+select * from facturas
+exec sumar_detalles_factura @id_factura=2
